f91bc9e233b4e3e9b73d9523cc9d45ed78276c4a
prdpta <pradiptaputramei01@gmail.com>
2024-05-14 18:54:06 +0700

    menambahkan class mahasiswa public dan public

---
+++ construktor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa
+{
+public:
+    int nim;
+    string nama;
+
+public:
+    Mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
