bbb8abe57a9952b9ef45028e613a8ab99471ebd9
prdpta <pradiptaputramei01@gmail.com>
2024-05-14 17:44:19 +0700

    menambahkan void mahasiswa

--- mbrStatik.cpp
+++ mbrStatik.cpp
@@ -20,3 +20,15 @@ public:
 };
 
 int mahasiswa::nim = 113;
+
+void mahasiswa::setID()
+{
+    id = nim++;
+}
+
+void mahasiswa::printAll()
+{
+    cout << "ID   = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
