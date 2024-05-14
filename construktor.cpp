ed3b5ac805abec0bd08b9443e166e8265cc8506e
prdpta <pradiptaputramei01@gmail.com>
2024-05-14 18:56:38 +0700

    menambahkan void cetak

--- construktor.cpp
+++ construktor.cpp
@@ -28,3 +28,9 @@ public:
         nim = iNim;
         nama = iNama;
     };
+    void cetak(){
+        cout << "NIM  = " << nim << endl;
+        cout << "Nama = " << nama << endl;
+        cout << endl;
+    };
+};
